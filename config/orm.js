0cca059c7776ce7d4b8b370f207044bfc7bf04c3
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-03-07 20:35:04 +1100

    update orm, server.js

--- config/orm.js
+++ config/orm.js
@@ -68,9 +68,23 @@ insertOne(table, cols, vals, cb){
     connection(result)
 
 },
-updateOne = () => {
+updateOne(table, objColVals, condition, cb){
+    let queryString = "UPDATE " + table;
 
+    queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+    connection.query(queryString, function (err, result){
+        if(err) throw err;
+    })
+    cb(result);
+
+
+},
 }
-
 
 module.exports = orm;
