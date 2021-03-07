9c9fa326565f5d19dd99e81c68a6b8af3d081697
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-03-07 12:53:09 +1100

    update controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,37 @@
-const { Router } = require('express');
 const express = require('express');
-const connect = require('../models/burger');
-
+const burger = require('../models/burger');
+const router = express.Router();
 //create the router app
-Router.length("/", function(req, res){
-    
-})
+
+router.get("/", function(req, res){
+    burger.selectAll(function (data){
+        const hbsObject = {
+            burgers: data,
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject)
+    })
+});
+
+router.post('/api.burgers', (req, res) => {
+    burger.insertOne([
+        'burger_name'
+    ],
+    [req.body.burger_name],
+    (result) => {
+        res.json({result})
+    })
+});
+
+router.put("/api/burgers/devoured/:id", function (req,res) {
+    const condition = `id = ${req.params.id}`;
+    console.log('conditon', condition);
+
+    burger.updateOne(
+        { devoured: true
+        }, conditon, function(data){
+            res.redirect("/");
+        });
+});
+
+module.exports = router
